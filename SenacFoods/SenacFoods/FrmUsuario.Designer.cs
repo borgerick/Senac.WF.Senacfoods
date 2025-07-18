cd12c3dfb5088844fd928ee9ecfe284e82cb860b
ERICKBORGESRODRIGUES <ERICKBORGESRODRIGUES@SCS217791>
2025-07-17 21:52:01 -0300

    TELAS USUARIO CAD E USUARIO

--- SenacFoods/SenacFoods/FrmUsuario.Designer.cs
+++ SenacFoods/SenacFoods/FrmUsuario.Designer.cs
@@ -31,12 +31,21 @@
             panel1 = new Panel();
             btnVoltar = new Button();
             txtTextoCardapio = new TextBox();
+            groupBox1 = new GroupBox();
+            btnAdicionarUsuario = new Button();
+            dataGridView1 = new DataGridView();
+            txtPesquisaUsuario = new TextBox();
+            btnExcluirUsuario = new Button();
+            btnEditarUsuario = new Button();
             panel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(192, 64, 0);
+            panel1.Controls.Add(txtPesquisaUsuario);
             panel1.Controls.Add(btnVoltar);
             panel1.Controls.Add(txtTextoCardapio);
             panel1.Location = new Point(0, 0);
@@ -67,11 +76,71 @@
             txtTextoCardapio.TabIndex = 2;
             txtTextoCardapio.Text = "USUÁRIO";
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnExcluirUsuario);
+            groupBox1.Controls.Add(btnAdicionarUsuario);
+            groupBox1.Controls.Add(btnEditarUsuario);
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Location = new Point(28, 101);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(843, 324);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Usuarios";
+            // 
+            // btnAdicionarUsuario
+            // 
+            btnAdicionarUsuario.BackColor = Color.FromArgb(192, 64, 0);
+            btnAdicionarUsuario.ForeColor = Color.BlanchedAlmond;
+            btnAdicionarUsuario.Location = new Point(15, 27);
+            btnAdicionarUsuario.Name = "btnAdicionarUsuario";
+            btnAdicionarUsuario.Size = new Size(112, 34);
+            btnAdicionarUsuario.TabIndex = 5;
+            btnAdicionarUsuario.Text = "Adicionar";
+            btnAdicionarUsuario.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 67);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(827, 236);
+            dataGridView1.TabIndex = 0;
+            // 
+            // txtPesquisaUsuario
+            // 
+            txtPesquisaUsuario.Location = new Point(245, 23);
+            txtPesquisaUsuario.Name = "txtPesquisaUsuario";
+            txtPesquisaUsuario.Size = new Size(370, 31);
+            txtPesquisaUsuario.TabIndex = 7;
+            // 
+            // btnExcluirUsuario
+            // 
+            btnExcluirUsuario.Location = new Point(139, 27);
+            btnExcluirUsuario.Name = "btnExcluirUsuario";
+            btnExcluirUsuario.Size = new Size(112, 34);
+            btnExcluirUsuario.TabIndex = 6;
+            btnExcluirUsuario.Text = "X Excluir";
+            btnExcluirUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarUsuario
+            // 
+            btnEditarUsuario.Enabled = false;
+            btnEditarUsuario.Location = new Point(257, 27);
+            btnEditarUsuario.Name = "btnEditarUsuario";
+            btnEditarUsuario.Size = new Size(112, 34);
+            btnEditarUsuario.TabIndex = 5;
+            btnEditarUsuario.Text = "# Editar";
+            btnEditarUsuario.UseVisualStyleBackColor = true;
+            // 
             // FrmUsuario
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(894, 447);
+            Controls.Add(groupBox1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FrmUsuario";
@@ -79,6 +148,8 @@
             Text = "FrmUsuario";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -87,5 +158,11 @@
         private Panel panel1;
         private Button btnVoltar;
         private TextBox txtTextoCardapio;
+        private GroupBox groupBox1;
+        private Button btnAdicionarUsuario;
+        private DataGridView dataGridView1;
+        private TextBox txtPesquisaUsuario;
+        private Button btnExcluirUsuario;
+        private Button btnEditarUsuario;
     }
 }
---
+++ SenacFoods/SenacFoods/FrmUsuarioCad.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace SenacFoods
+{
+    partial class FrmUsuarioCad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            btnCancelar = new Button();
+            btnSalvar = new Button();
+            textPreco = new TextBox();
+            txtTitulo = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            txtTextoCardapio = new TextBox();
+            panel1 = new Panel();
+            btnVoltar = new Button();
+            groupBox1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(btnCancelar);
+            groupBox1.Controls.Add(btnSalvar);
+            groupBox1.Controls.Add(textPreco);
+            groupBox1.Controls.Add(txtTitulo);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.ForeColor = Color.Black;
+            groupBox1.Location = new Point(12, 71);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(826, 317);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "btnSalvar";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(260, 112);
+            label4.Name = "label4";
+            label4.Size = new Size(140, 25);
+            label4.TabIndex = 11;
+            label4.Text = "Validação Senha";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(260, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(527, 31);
+            textBox1.TabIndex = 9;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.BackColor = Color.FromArgb(255, 128, 128);
+            btnCancelar.Location = new Point(299, 281);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(132, 36);
+            btnCancelar.TabIndex = 8;
+            btnCancelar.Text = "X Cancelar";
+            btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.BackColor = Color.FromArgb(128, 255, 128);
+            btnSalvar.Location = new Point(437, 281);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(132, 36);
+            btnSalvar.TabIndex = 7;
+            btnSalvar.Text = "# Salvar";
+            btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // textPreco
+            // 
+            textPreco.Location = new Point(27, 140);
+            textPreco.Name = "textPreco";
+            textPreco.Size = new Size(206, 31);
+            textPreco.TabIndex = 7;
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.Location = new Point(25, 63);
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.Size = new Size(208, 31);
+            txtTitulo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Senha:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(264, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 25);
+            label2.TabIndex = 1;
+            label2.Text = "E-mail";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nome Usuario";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(27, 215);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(185, 31);
+            textBox3.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(28, 187);
+            label5.Name = "label5";
+            label5.Size = new Size(115, 25);
+            label5.TabIndex = 12;
+            label5.Text = "Perfil Usuario";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(264, 140);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(206, 31);
+            textBox4.TabIndex = 14;
+            // 
+            // txtTextoCardapio
+            // 
+            txtTextoCardapio.BackColor = Color.FromArgb(192, 64, 0);
+            txtTextoCardapio.BorderStyle = BorderStyle.None;
+            txtTextoCardapio.Font = new Font("Segoe UI", 19F);
+            txtTextoCardapio.ForeColor = Color.White;
+            txtTextoCardapio.Location = new Point(10, 10);
+            txtTextoCardapio.Name = "txtTextoCardapio";
+            txtTextoCardapio.Size = new Size(281, 51);
+            txtTextoCardapio.TabIndex = 2;
+            txtTextoCardapio.Text = "CAD USUÁRIO";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 64, 0);
+            panel1.Controls.Add(btnVoltar);
+            panel1.Controls.Add(txtTextoCardapio);
+            panel1.Location = new Point(2, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(896, 64);
+            panel1.TabIndex = 11;
+            // 
+            // btnVoltar
+            // 
+            btnVoltar.FlatAppearance.BorderSize = 0;
+            btnVoltar.Location = new Point(724, 12);
+            btnVoltar.Name = "btnVoltar";
+            btnVoltar.Size = new Size(112, 34);
+            btnVoltar.TabIndex = 2;
+            btnVoltar.Text = "Voltar";
+            btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // FrmUsuarioCad
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(850, 400);
+            Controls.Add(panel1);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmUsuarioCad";
+            Text = "FrmUsuarioCad";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnCancelar;
+        private Button btnSalvar;
+        private TextBox textPreco;
+        private TextBox txtTitulo;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox txtTextoCardapio;
+        private Panel panel1;
+        private Button btnVoltar;
+    }
+}
